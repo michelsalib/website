460f8f2a322b1fcbe795649226ea09cc033f72a2
Michel Salib <michelsalib@hotmail.com>
2014-09-08 19:25:07 +0200

    Attemp to fix tsd (temp)

---
+++ typings/tsd.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="request/request.d.ts" />
+/// <reference path="q/Q.d.ts" />
+/// <reference path="express/express.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="node/node.d.ts" />
+/// <reference path="form-data/form-data.d.ts" />
