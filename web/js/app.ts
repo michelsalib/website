68181c6040a8452e8543d6d5b10ca1f6a042d1d1
Michel Salib <michelsalib@hotmail.com>
2014-09-25 22:07:37 +0200

    TV homepage

--- web/js/app.ts
+++ web/js/app.ts
@@ -66,7 +66,7 @@ module MichelSalib {
     }
 }
 
-var app = angular.module('michel-salib', ['ngRoute', 'ngCookies']);
+var app = angular.module('michel-salib', ['ngAnimate', 'ngRoute', 'ngCookies']);
 
 app.controller('AppController', MichelSalib.Controllers.AppController);
 
