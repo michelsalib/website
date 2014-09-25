6fa218d44663adce9cee0eb77cd72e52a5c7b3de
Michel Salib <michelsalib@hotmail.com>
2014-09-25 17:44:31 +0200

    More content

--- web/js/app.ts
+++ web/js/app.ts
@@ -19,8 +19,11 @@ module MichelSalib {
         export interface IShowScope extends IAppScope {
             show: string;
             info: any;
+            subtitles: any;
+            torrents: any;
             error: any;
             loading: boolean;
+            episodeMatcher(season, episode): (item) => boolean;
         }
 
         export class AppController {
@@ -46,6 +49,14 @@ module MichelSalib {
                     .then(r => $scope.info = r.data)
                     .catch(err => $scope.error = err)
                     .finally(() => $scope.loading = false);
+
+                $http.get('/torrents/' + $scope.show)
+                    .then(r => $scope.torrents = r.data);
+
+                $http.get('/subtitles/' + $scope.show)
+                    .then(r => $scope.subtitles = r.data);
+
+                $scope.episodeMatcher = (season, episode) => (item) => item.season == season && item.episode == episode;
             }
         }
     }
